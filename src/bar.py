dba85ac8fc4a9daddc6d7b933c861c6afdbbe816
SShingler <s.shingler@hotmail.co.uk>
2023-02-04 21:17:28 +0000

    10 - bar/rooms/songs/guests - add rooms to bar, and songs to bar

--- src/bar.py
+++ src/bar.py
@@ -6,4 +6,18 @@ class Bar:
         self.total_song_list = []
         self.total_guest_list = []
 
+    def add_room_to_total_room_list(self, room):
+        self.total_room_list.append(room)
+
+    def length_of_total_room_list (self):
+        return len(self.total_room_list)
+
+    def add_song_to_total_song_list (self, song):
+        self.total_song_list.append(song)
+
+    def length_of_total_song_list (self):
+        return len(self.total_song_list)
+
     
+    
+
--- tests/bar_test.py
+++ tests/bar_test.py
@@ -7,6 +7,10 @@ from src.room import Room
 class TestBar(unittest.TestCase):
     def setUp(self):
         self.bar = Bar ("Bar One", 1000)
+        self.room_1 = Room ("Rock Room")
+        self.room_2 = Room ("Dance Room") 
+        self.song_1 = Song ("Back in Black", "ACDC", "Rock")
+        self.song_2 = Song ("Dance Song", "MR DJ", "Dance")
 
     def test_bar_has_name(self):
         expected = "Bar One"
@@ -17,6 +21,27 @@ class TestBar(unittest.TestCase):
         expected = 1000 
         actual = self.bar.till
         self.assertEqual(expected, actual)
+
+    def test_bar_has_total_room_list(self):
+        expected = []
+        actual = self.bar.total_room_list
+        self.assertEqual(expected, actual)
+
+    def test_add_rooms_to_total_room_list(self): 
+        self.bar.add_room_to_total_room_list(self.room_1)
+        self.bar.add_room_to_total_room_list(self.room_2)
+        expected = 2
+        actual = self.bar.length_of_total_room_list()
+        self.assertEqual (expected, actual)
+
+    def test_add_song_to_total_song_list(self):
+        self.bar.add_song_to_total_song_list(self.song_1)
+        self.bar.add_song_to_total_song_list(self.song_2)
+        expected = 2
+        actual = self.bar.length_of_total_song_list()
+        self.assertEqual (expected, actual)
+
+
 
 
 
