d88992533e6edff1948f001ff075de0f019da5c8
SShingler <s.shingler@hotmail.co.uk>
2023-02-04 14:54:40 +0000

    five - song & guest objects set up

--- src/guest.py
+++ src/guest.py
@@ -1,3 +1,6 @@
 class Guest:
-    def __init__(self, name):
+    def __init__(self, name, wallet, fave_song):
         self.name = name
+        self.wallet = wallet
+        self.fave_song = fave_song
+
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,11 +3,23 @@ from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest = Guest("Rockin Ralph")
+        self.guest = Guest("Rockin Ralph", 100, "Back in Black")
 
 
-    def test_has_name(self):
+    def test_guest_has_name(self):
         expected = "Rockin Ralph"
         actual = self.guest.name
         self.assertEqual (expected, actual)
+
+    def test_guest_has_wallet(self):
+        expected = 100
+        actual = self.guest.wallet
+        self.assertEqual (expected, actual)
+
+    def test_guest_has_fave_song(self):
+        expected = "Back in Black"
+        actual = self.guest.fave_song
+        self.assertEqual (expected, actual)
+
+        
 
